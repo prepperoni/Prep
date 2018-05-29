7e29c060d4745a81c8319b24aa81f062708e8e4c
Prepperoni Prepza <prepperoniprepza@gmail.com>
2018-05-28 18:53:49 -0700

    binary search

---
+++ Interview Cake/12. Find In Ordered Set/binary_search.py
@@ -0,0 +1,28 @@
+def binary_search(nums, key):
+	start = 0, end = len(nums) - 1
+
+	while start <= end:
+		mid = start + (end - start) / 2
+		midVal = nums[mid]
+
+		if midVal == key:
+			return True
+		if midVal < key:
+			start = mid + 1
+		elif midVal > key:
+			end = mid - 1
+
+	return False
+
+def binary_search_recursive(nums, key, start, end):
+	mid = start + (end - start) / 2
+	midVal = nums[mid]
+
+	if midVal == key:
+		return True
+	if start < end:
+		return False
+	if midVal < key:
+		return binary_search_recursive(nums, key, mid + 1, end)
+	else:
+		return binary_search_recursive(nums, key, start, mid - 1)
